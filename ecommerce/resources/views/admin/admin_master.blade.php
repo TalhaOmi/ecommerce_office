7448d1bf990a8b8fe4c0e521c56e87b73d8e9023
TalhaOmi <talha.talukder@northsouth.edu>
2022-02-06 21:12:07 +0600

    product v2

--- ecommerce/resources/views/admin/admin_master.blade.php
+++ ecommerce/resources/views/admin/admin_master.blade.php
@@ -54,7 +54,7 @@
 	 
 	<!-- Vendor JS -->
 	<script src="{{ asset('backend/js/vendors.min.js') }}"></script>
-    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>	
+  <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>	
 	<script src="{{ asset('../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
@@ -62,6 +62,14 @@
   <script src="{{ asset('../assets/vendor_components/datatable/datatables.min.js') }}"></script>
 	<script src="{{ asset('backend/js/pages/data-table.js') }}"></script>
 	
+<!-- Tags input script -->
+  <script src="{{ asset('../assets/vendor_components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js') }}"></script>
+
+<!-- // CK EDITOR  --> 
+  <script src="{{ asset('../assets/vendor_components/ckeditor/ckeditor.js') }}"></script>
+  <script src="{{ asset('../assets/vendor_plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.js') }}"></script>
+  <script src="{{ asset('backend/js/pages/editor.js') }}"></script>
+
 	<!-- Sunny Admin App -->
 	<script src="{{ asset('backend/js/template.js') }}"></script>
 	<script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
