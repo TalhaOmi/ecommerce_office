07959eb4d1dd860d5e966d43113b9a7918755f75
TalhaOmi <talha.talukder@northsouth.edu>
2022-03-07 23:11:13 +0600

    category in frontend

--- ecommerce/app/Http/Controllers/Backend/CategoryController.php
+++ ecommerce/app/Http/Controllers/Backend/CategoryController.php
@@ -52,11 +52,9 @@ class CategoryController extends Controller
 
     }
 
-    public function CategoryUpdate(Request $request){
+    public function CategoryUpdate(Request $request ,$id){
         
-        $cat_id = $request->id;
-
-        Category::findOrFail($cat_id)->update([
+        Category::findOrFail($id)->update([
             'category_name_en' => $request->category_name_en,
             'category_name_bn' => $request->category_name_bn,
             'category_slug_en' => strtolower(str_replace(' ','-',$request->category_name_en)),
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -101,7 +101,7 @@ Route::prefix('category')->group(function(){
 
     Route::get('/edit/{id}', [CategoryController::class, 'CategoryEdit'])->name('category.edit');
 
-    Route::post('/update', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
+    Route::post('/update/{id}', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
     
     Route::get('/delete/{id}', [CategoryController::class, 'CategoryDelete'])->name('category.delete');
 
