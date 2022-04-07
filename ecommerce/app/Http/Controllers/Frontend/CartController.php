769378595d8a766f529035875e9f533dae20aa5c
TalhaOmi <talha.talukder@northsouth.edu>
2022-04-08 03:32:59 +0600

    129

--- ecommerce/app/Http/Controllers/Frontend/CartController.php
+++ ecommerce/app/Http/Controllers/Frontend/CartController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Auth;
+use App\Models\Wishlist;
+use Carbon\Carbon;
 
 class CartController extends Controller
 {
@@ -59,7 +62,7 @@ class CartController extends Controller
     	return response()->json(array(
     		'carts' => $carts,
     		'cartQty' => $cartQty,
-    		'cartTotal' => ($cartTotal),
+    		'cartTotal' => round($cartTotal),
 
     	));
     } // end method 
@@ -71,6 +74,37 @@ class CartController extends Controller
     	return response()->json(['success' => 'Product Remove from Cart']);
 
     } // end mehtod 
+
+
+    // add to wishlist mehtod 
+
+    public function AddToWishlist(Request $request, $product_id){
+
+        if (Auth::check()) {
+
+            $exists = Wishlist::where('user_id',Auth::id())->where('product_id',$product_id)->first();
+
+            if (!$exists) {
+               Wishlist::insert([
+                'user_id' => Auth::id(), 
+                'product_id' => $product_id, 
+                'created_at' => Carbon::now(), 
+            ]);
+           return response()->json(['success' => 'Successfully Added On Your Wishlist']);
+
+            }else{
+
+                return response()->json(['error' => 'This Product has Already on Your Wishlist']);
+
+            }            
+            
+        }else{
+
+            return response()->json(['error' => 'At First Login Your Account']);
+
+        }
+
+    } // end method 
 
 
 }
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -14,6 +14,8 @@ use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\LanguageController;
 use App\Http\Controllers\Frontend\CartController;
+use App\Http\Controllers\User\WishlistController;
+use App\Http\Controllers\User\CartPageController;
 
 
 
@@ -221,3 +223,30 @@ Route::get('/product/mini/cart/', [CartController::class, 'AddMiniCart']);
 
 // Remove mini cart
 Route::get('/minicart/product-remove/{rowId}', [CartController::class, 'RemoveMiniCart']);
+
+// Add to Wishlist
+Route::post('/add-to-wishlist/{product_id}', [CartController::class, 'AddToWishlist']); 
+
+
+Route::group(['prefix'=>'user','middleware' => ['user','auth'],'namespace'=>'User'],function(){
+
+    // Wishlist page
+    Route::get('/wishlist', [WishlistController::class, 'ViewWishlist'])->name('wishlist');
+    
+    Route::get('/get-wishlist-product', [WishlistController::class, 'GetWishlistProduct']);
+    
+    Route::get('/wishlist-remove/{id}', [WishlistController::class, 'RemoveWishlistProduct']);
+     
+    
+    });
+    
+// My Cart Page All Routes
+Route::get('/mycart', [CartPageController::class, 'MyCart'])->name('mycart');
+
+Route::get('/user/get-cart-product', [CartPageController::class, 'GetCartProduct']);
+    
+Route::get('/user/cart-remove/{rowId}', [CartPageController::class, 'RemoveCartProduct']);
+
+Route::get('/cart-increment/{rowId}', [CartPageController::class, 'CartIncrement']);
+
+Route::get('/cart-decrement/{rowId}', [CartPageController::class, 'CartDecrement']);
