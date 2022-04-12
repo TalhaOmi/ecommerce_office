e9cb9e689fc86edc522759d7f21de03a61abb3af
TalhaOmi <talha.talukder@northsouth.edu>
2022-04-12 12:16:06 +0600

    146

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -311,4 +311,14 @@ Route::prefix('shipping')->group(function(){
     
    });
 
+   // Frontend Coupon Option
+
+Route::post('/coupon-apply', [CartController::class, 'CouponApply']);
+
+Route::get('/coupon-calculation', [CartController::class, 'CouponCalculation']);
+
+Route::get('/coupon-remove', [CartController::class, 'CouponRemove']);
+
+
+
 
